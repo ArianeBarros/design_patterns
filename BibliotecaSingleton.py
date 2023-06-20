bdfb11e3ef5670110ade7e0ab8d4388f7ab7082d
ARIANE PAULA BARROS <ariane.barros@ifood.com.br>
2023-06-19 21:40:57 -0300

    fix BibliotecaSingleton

--- BibliotecaSingleton.py
+++ BibliotecaSingleton.py
@@ -24,11 +24,11 @@ class BibliotecaSingleton():
         self.usuarios = []
 
     def adicionarLivro(self, livro):
-        if livro.estaDisponivel():
+        if livro not in self.livros:
             self.livros.append(livro)
             print(f"Livro '{livro.obterTitulo()}' adicionado à biblioteca.")
         else:
-            print(f"Livro '{livro.obterTitulo()}' não está disponível.")
+            print(f"Livro '{livro.obterTitulo()}' já adicionado.")
 
     def removerLivro(self, livro):
         if livro in self.livros:
@@ -40,15 +40,14 @@ class BibliotecaSingleton():
     def buscarLivro(self, titulo):
         for livro in self.livros:
             if livro.obterTitulo() == titulo:
-                if livro.obterTitulo() == titulo:
                     return livro
         return None
 
     def adicionarUsuario(self, usuario):
-        if usuario.obterNome() != "":
+        try:
             self.usuarios.append(usuario)
             print(f"Usuário '{usuario.obterNome()}' adicionado à biblioteca.")
-        else:
+        except:
             print("Nome de usuário inválido.")
 
     def removerUsuario(self, usuario):
@@ -61,23 +60,18 @@ class BibliotecaSingleton():
     def buscarUsuario(self, nome):
         for usuario in self.usuarios:
             if usuario.obterNome() == nome:
-                if usuario.obterNome() == nome:
-                    return usuario
+                return usuario
         return None
    
     def emprestarLivro(self, livro):
-        if livro.estaDisponivel():
-            self.livros.remove(livro)
-            livro.atualizarStatus(False)
-            self.livros.append(livro)
+        if livro in self.livros and livro.estaDisponivel():
+            self.buscarLivro(livro).atualizarStatus(False)
             return 1
         return 0
 
     def receberLivro(self, livro):
         if livro.estaDisponivel() == False:
-           self.livros.remove(livro)
-           livro.atualizarStatus(True)
-           self.livros.append(livro)
+           self.buscarLivro(livro).atualizarStatus(True)
            return 1
         return 0
 
